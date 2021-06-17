171f91b55659fbaa09933e47f118c2def53ff45b
Ashish Srivastav <67631534+ashish-ash303@users.noreply.github.com>
2021-06-18 00:20:10 +0530

    Add files via upload

---
+++ whatappmsg.py
@@ -0,0 +1,6 @@
+import pywhatkit as kit
+# kit.sendwhatmsg('+[Number With Country Code ]', "Hii", 23, 54) Used to send automatic Code
+# kit.playonyt("Python ") Used to Play Video on Youtube
+# kit.search("pywhatkit") used to Search On Google
+# kit.info("Python") Used to get infomation On Any Topic
+# kit.text_to_handwriting("Python is an interpreted high-level general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.Python is dynamically-typed and garbage-collected.") Used to Convert into text_to_handwriting
